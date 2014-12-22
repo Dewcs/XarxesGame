a35deda2acdd68c7e5eabed3e35983155a6bd45e
Dewcs <andreualien@hotmail.com>
2014-12-22 12:21:06 +0100

    Client wtf

--- XGame/Client/client.cpp
+++ XGame/Client/client.cpp
@@ -9,6 +9,7 @@
 //
 
 #include <iostream>
+#include <thread>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
 
@@ -16,35 +17,74 @@
 
 using boost::asio::ip::udp;
 
-int main(int argc, char* argv[])
-{
+boost::asio::io_service io_service;
+udp::socket *sock=NULL;
+udp::endpoint server_endpoint;
+
+enum action_t {
+	ACTION_OPEN_GAME,
+	ACTION_BEGIN_GAME,
+	ACTION_END_LEVEL,
+	ACTION_LOSE_LIFE,
+	ACTION_BUY_ITEM,
+	ACTION_END_GAME,
+};
+
+void send(action_t action, unsigned value) {
 	try
 	{
+		std::cout << "SENDING " << sock << std::endl;
+		boost::array<char, 2> send_buf = { (char)action, (char)value };
+		sock->send_to(boost::asio::buffer(send_buf), server_endpoint);
+		std::cout << "SENDED" << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
 
-		boost::asio::io_service io_service;
+bool connect() {
+	try
+	{
+		std::cout << "CONNECTING" << std::endl;
+		
 
 		udp::resolver resolver(io_service);
 		udp::resolver::query query("127.0.0.1", PORT);
-		udp::endpoint receiver_endpoint = *resolver.resolve(query);
+		server_endpoint = *resolver.resolve(query);
 
 		udp::socket socket(io_service);
 		socket.open(udp::v4());
-
-		boost::array<char, 2> send_buf = { 'a','b' };
-		socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
-		socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
+		sock = &socket;
+		
+		boost::array<char, 2> send_buf = { 0, 1 };
+		sock->send_to(boost::asio::buffer(send_buf), server_endpoint);
+		/*socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
 
 		boost::array<char, 128> recv_buf;
 		udp::endpoint sender_endpoint;
 		size_t len = socket.receive_from(
 			boost::asio::buffer(recv_buf), sender_endpoint);
 
-		std::cout.write(recv_buf.data(), len);
+		std::cout.write(recv_buf.data(), len);*/
 	}
 	catch (std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
+		return false;
 	}
+	return true;
+}
 
+int main(int argc, char* argv[])
+{
+	if (connect()) {
+		std::cout << "CONNECTED" << std::endl;
+		send(ACTION_BEGIN_GAME, 0);
+	}
+	else {
+
+	}
 	return 0;
 }
