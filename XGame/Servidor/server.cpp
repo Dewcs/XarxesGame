b94a76fc3558caa62035a1b976d0ffde4d603994
Dewcs <andreualien@hotmail.com>
2014-12-22 14:22:12 +0100

    Server v0

--- XGame/Servidor/server.cpp
+++ XGame/Servidor/server.cpp
@@ -10,12 +10,60 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <queue>
+#include <string>
+#include <sstream>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
+#include <unordered_map>
+#include <fstream>
 
 #define PORT 51116
+#define FNAME "log.txt"
 
 using boost::asio::ip::udp;
+
+enum action_t {
+	ACTION_OPEN_GAME,
+	ACTION_BEGIN_GAME,
+	ACTION_END_LEVEL,
+	ACTION_LOSE_LIFE,
+	ACTION_BUY_ITEM,
+	ACTION_END_GAME,
+};
+
+std::string action2str(action_t action) {
+	switch (action)
+	{
+	case ACTION_OPEN_GAME:
+		return "OPEN_GAME";
+		break;
+	case ACTION_BEGIN_GAME:
+		return "BEGIN_GAME";
+		break;
+	case ACTION_END_LEVEL:
+		return "END_LEVEL";
+		break;
+	case ACTION_LOSE_LIFE:
+		return "LOSE_LIFE";
+		break;
+	case ACTION_BUY_ITEM:
+		return "BUY_ITEM";
+		break;
+	case ACTION_END_GAME:
+		return "END_GAME";
+		break;
+	default:
+		break;
+	}
+}
+
+struct informacio {
+	enum action_t action;
+	unsigned value;
+};
+
+typedef std::unordered_map<std::string, std::queue<informacio> > infomap;
 
 class server
 {
@@ -24,7 +72,6 @@ public:
 		: io_service_(io_service),
 		socket_(io_service, udp::endpoint(udp::v4(), port))
 	{
-		std::cout << "0" << std::endl;
 		socket_.async_receive_from(
 			boost::asio::buffer(data_, max_length), sender_endpoint_,
 			boost::bind(&server::handle_receive_from, this,
@@ -37,15 +84,26 @@ public:
 	{
 		if (!error && bytes_recvd > 0)
 		{
-			std::cout << "1 " << bytes_recvd << std::endl;
+			informacio info = { ACTION_OPEN_GAME, 0 };
 			for (int i = 0; i < bytes_recvd; ++i) {
-				std::cout << (int)data_[i] << std::endl;
+				if (i == 0) info.action = (action_t)data_[i];
+				else if (i < 5) {
+					info.value |= (action_t)data_[i]<<8*(i-1);
+				}
 			}
-			std::cout << sender_endpoint_ << std::endl;
-			std::cout << std::endl;
-			socket_.async_send_to(
-				boost::asio::buffer(data_, bytes_recvd), sender_endpoint_,
-				boost::bind(&server::handle_send_to, this,
+			std::stringstream ss;
+			ss << sender_endpoint_;
+			std::string key = ss.str();
+			userinfo[key].push(info);
+			if (info.action == ACTION_END_GAME) {
+				store2log(key,userinfo[key]);
+			}
+			dumpMem();
+
+			// wait for next
+			socket_.async_receive_from(
+				boost::asio::buffer(data_, max_length), sender_endpoint_,
+				boost::bind(&server::handle_receive_from, this,
 				boost::asio::placeholders::error,
 				boost::asio::placeholders::bytes_transferred));
 		}
@@ -60,14 +118,38 @@ public:
 		}
 	}
 
+	void dumpMem() {
+		std::cout << "################### DUMP #########################" << std::endl;
+		for (auto& x : userinfo) {
+			std::cout <<"user " << x.first << std::endl;
+			for (int i = 0; i < x.second.size(); ++i) {
+				informacio info = x.second.front();
+				std::cout << info.action << " " << info.value << std::endl;
+				x.second.pop();
+				x.second.push(info);
+			}
+		}
+		std::cout << "################### END DUMP #####################" << std::endl;
+	}
+
+	void store2log(std::string user, std::queue<informacio> q) {
+		std::fstream fs;
+		fs.open(FNAME, std::fstream::out | std::fstream::app);
+		while (!q.empty()) {
+			informacio top = q.front();
+			fs << "User: " << user << ", Action: " << action2str(top.action) << ", Value: " << top.value << std::endl;
+			q.pop();
+		}
+		fs.close();
+	}
+
 	void handle_send_to(const boost::system::error_code& error, size_t bytes_sent)
 	{
-		std::cout << "3" << std::endl;
-		socket_.async_receive_from(
+		/*socket_.async_receive_from(
 			boost::asio::buffer(data_, max_length), sender_endpoint_,
 			boost::bind(&server::handle_receive_from, this,
 			boost::asio::placeholders::error,
-			boost::asio::placeholders::bytes_transferred));
+			boost::asio::placeholders::bytes_transferred));*/
 	}
 
 private:
@@ -76,18 +158,17 @@ private:
 	udp::endpoint sender_endpoint_;
 	enum { max_length = 1024 };
 	char data_[max_length];
+	infomap userinfo;
 };
 
 int main(int argc, char* argv[])
 {
 	try {
 		boost::asio::io_service io_service;
-
-		using namespace std; // For atoi.
 		server s(io_service, PORT);
-		std::cout << "-1" << std::endl;
+		std::cout << "BEGIN SERVER" << std::endl;
 		io_service.run();
-		std::cout << "-2" << std::endl;
+		std::cout << "END SERVER" << std::endl;
 	}
 	catch (std::exception& e)
 	{
